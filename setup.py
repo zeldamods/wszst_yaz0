426bd4e242fb3da61651fa5b3f67bcd28674d2a4
Léo Lam <leo@innovatetechnologi.es>
2018-07-30 17:41:52 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="wszst_yaz0",
+    version="1.0.0",
+    author="leoetlino",
+    author_email="leo@leolam.fr",
+    description="Quick and dirty wrapper around wszst",
+    url="https://github.com/leoetlino/wszst_yaz0",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Topic :: Software Development :: Libraries",
+    ],
+    python_requires='>=3.6',
+)
