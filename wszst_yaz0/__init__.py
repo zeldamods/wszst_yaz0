1855466e1a522d753c3538fe36caa10664ff0083
Léo Lam <leo@innovatetechnologi.es>
2019-04-22 20:26:44 +0200

    Bump size limit

--- wszst_yaz0/__init__.py
+++ wszst_yaz0/__init__.py
@@ -7,13 +7,13 @@ from typing import Union
 _tool_name = 'wszst' if os.name != 'nt' else 'wszst.exe'
 
 def compress(data: Union[bytes, memoryview], level: int = 10) -> bytes:
-    return subprocess.run([_tool_name, "comp", "-", "-d-", f"-C{level}"], input=data, # type: ignore
+    return subprocess.run([_tool_name, "comp", "-", "-d-", f"-C{level}", "-M1000"], input=data, # type: ignore
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True).stdout
 
 def decompress(data: Union[bytes, memoryview]) -> bytes:
-    return subprocess.run([_tool_name, "de", "-", "-d-"], input=data, # type: ignore
+    return subprocess.run([_tool_name, "de", "-", "-d-", "-M1000"], input=data, # type: ignore
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True).stdout
 
 def decompress_file(file_path: str) -> bytes:
-    return subprocess.run([_tool_name, "de", file_path, "-d-"],
+    return subprocess.run([_tool_name, "de", file_path, "-d-", "-M1000"],
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True).stdout
